7eaa00fcd45317f97169fd5cf3e10780ad04ac6d
Piyanshu Mangla <piyanshumangla456@gmail.com>
2021-02-06 13:54:23 +0530

    rendering users signIn and signUp page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,4 +2,20 @@ module.exports.profile = function(req, res){
     return res.render('user_profile', {
         title: 'Profile'
     });
+};
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up', {
+        title: 'Sign Up'
+    });
+};
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in', {
+        title: 'Sign In'
+    });
+};
+module.exports.create = function(req, res){
+    // TODO LATER
+}
+module.exports.createSession = function(req, res){
+    // TODO LATER
 }
