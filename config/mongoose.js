7bcf7f4b38f24b5c0309cc0e836a43579e029411
Piyanshu Mangla <piyanshumangla456@gmail.com>
2021-02-06 10:05:37 +0530

    Setting up the configuration for mongoose

---
+++ config/mongoose.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/codeial_development');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'error in connecting db'));
+db.once('open', function(){
+    console.log('Successfully connected to db');
+});
