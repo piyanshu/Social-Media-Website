d83aeb979b9600c0e7f754fb44106087311fb921
Piyanshu Mangla <piyanshumangla456@gmail.com>
2021-02-07 00:21:04 +0530

    creating controllerr for sign-up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,4 @@
+const User = require('../models/user');
 module.exports.profile = function(req, res){
     return res.render('user_profile', {
         title: 'Profile'
@@ -14,7 +15,21 @@ module.exports.signIn = function(req, res){
     });
 };
 module.exports.create = function(req, res){
-    // TODO LATER
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('Error in finding user'); return;}
+        if(!user){
+            User.create(req.body, function(err, user){
+                if(err){console.log('Error in finding user'); return;}
+
+                return res.redirect('/users/sign-in');
+            })
+        }else{
+            return res.redirect('back');
+        }
+    })
 }
 module.exports.createSession = function(req, res){
     // TODO LATER
