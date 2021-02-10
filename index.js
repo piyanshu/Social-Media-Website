a903377969aa81ef5c4cdd15f599435973d2badf
Piyanshu Mangla <piyanshumangla456@gmail.com>
2021-02-11 03:10:38 +0530

    setting the current authenticated user

--- index.js
+++ index.js
@@ -33,6 +33,8 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(setAuthenticatedUser);
+
 app.use('/', require('./routes'));
 
 app.listen(port, function(err){
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const passport = require('passport');
 const router = express.Router();
 const usersController = require('../controllers/users_controller');
 
-router.get('/profile', usersController.profile);
+router.get('/profile', passport.checkAuthentication, usersController.profile);
 router.get('/sign-up', usersController.signUp);
 router.get('/sign-in', usersController.signIn);
 router.post('/create', usersController.create);
@@ -11,5 +11,4 @@ router.post('/create-session', passport.authenticate(
 	'local',
 	{failureRedirect: '/users/sign-in'},
 ), usersController.createSession);
-// router.post('/create-session', usersController.createSession);
 module.exports = router;
