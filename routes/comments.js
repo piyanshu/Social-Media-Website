841db9f3ac7ebf1956f13d2f1e0c9aa0583de566
Piyanshu Mangla <piyanshumangla456@gmail.com>
2021-02-17 00:06:12 +0530

    adding comments to db

---
+++ routes/comments.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+const commentsController = require('../controllers/comments_controller');
+router.post('/create', passport.checkAuthentication, commentsController.create);
+module.exports = router;
