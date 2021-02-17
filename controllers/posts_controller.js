d48483e65cc479f9c37c0aa99afb72213b0a0b09
Piyanshu Mangla <piyanshumangla456@gmail.com>
2021-02-18 02:28:53 +0530

    deleting a post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment'); 
 module.exports.create = function(req, res){
     Post.create({
         content: req.body.content,
@@ -10,4 +11,26 @@ module.exports.create = function(req, res){
         }
         return res.redirect('back');
     });
+}
+module.exports.destroy = function(req, res){
+    Post.findById(req.params.id, function(err, post){
+        if(err){
+            console.log('error in finding the post');
+            return;
+        }
+        // .id means converting the object id into string
+        if(post.user == req.user.id){
+            post.remove();
+
+            Comment.deleteMany({post: req.params.id}, function(err){
+                if(err){
+                    console.log('error in deleting the post');
+                    return;
+                }
+                return res.redirect('back');
+            });
+        }else{
+            return res.redirect('back');
+        }
+    })
 }
--- routes/posts.js
+++ routes/posts.js
@@ -3,4 +3,5 @@ const passport = require('passport');
 const router = express.Router();
 const postsController = require('../controllers/posts_controller');
 router.post('/create', passport.checkAuthentication, postsController.create);
+router.get('/destroy/:id', passport.checkAuthentication, postsController.destroy);
 module.exports = router;
