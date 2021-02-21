1ff3024a39cc24ff206c08e3fe21256c08a4f364
Piyanshu Mangla <piyanshumangla456@gmail.com>
2021-02-22 00:09:02 +0530

    adding flash messages to the users controller

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,69 +1,89 @@
 const User = require('../models/user');
+
+// let's keep it same as before
 module.exports.profile = function(req, res){
     User.findById(req.params.id, function(err, user){
-        if(err){
-            console.log('Error in finding user');
-            return;
-        }
         return res.render('user_profile', {
             title: 'User Profile',
             profile_user: user
         });
-    })
-};
+    });
+
+}
+
+
 module.exports.update = function(req, res){
-    if(req.params.id == req.user.id){
+    if(req.user.id == req.params.id){
         User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
-            if(err){
-                console.log('Error in updating the user');
-            }
+            req.flash('success', 'Updated!');
             return res.redirect('back');
         });
     }else{
-        return res.status(401).send('Unauthorised');
+        req.flash('error', 'Unauthorized!');
+        return res.status(401).send('Unauthorized');
     }
-};
+}
+
+
+// render the sign up page
 module.exports.signUp = function(req, res){
-    if(req.isAuthenticated()){
+    if (req.isAuthenticated()){
         return res.redirect('/users/profile');
     }
+
+
     return res.render('user_sign_up', {
-        title: 'User Sign Up'
-    });
-};
+        title: "Codeial | Sign Up"
+    })
+}
+
+
+// render the sign in page
 module.exports.signIn = function(req, res){
-    if(req.isAuthenticated()){
+
+    if (req.isAuthenticated()){
         return res.redirect('/users/profile');
     }
     return res.render('user_sign_in', {
-        title: 'User Sign In'
-    });
-};
+        title: "Codeial | Sign In"
+    })
+}
+
+// get the sign up data
 module.exports.create = function(req, res){
-    if(req.body.password != req.body.confirm_password){
+    if (req.body.password != req.body.confirm_password){
+        req.flash('error', 'Passwords do not match');
         return res.redirect('back');
     }
+
     User.findOne({email: req.body.email}, function(err, user){
-        if(err){console.log('Error in finding user'); return;}
-        if(!user){
+        if(err){req.flash('error', err); return}
+
+        if (!user){
             User.create(req.body, function(err, user){
-                if(err){console.log('Error in finding user'); return;}
+                if(err){req.flash('error', err); return}
 
                 return res.redirect('/users/sign-in');
             })
         }else{
+            req.flash('success', 'You have signed up, login to continue!');
             return res.redirect('back');
         }
-    })
+
+    });
 }
-// sign-in and create a session for user
+
+
+// sign in and create a session for the user
 module.exports.createSession = function(req, res){
     req.flash('success', 'Logged in Successfully');
+    return res.redirect('/');
+}
+
+module.exports.destroySession = function(req, res){
+    req.logout();
+    req.flash('success', 'You have logged out!');
+
 
     return res.redirect('/');
 }
-module.exports.destroySession = function(req, res){
-    req.flash('success', 'You have logged out!');
-    req.logout();
-    return res.redirect('/');
-};
